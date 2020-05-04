8cb3328a2f4d74989a10f91fbc29463c030981b6
Mark Gridley <mark_gridley@yahoo.com>
2020-05-04 16:28:54 -0700

    write all tests, write all logic

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -4,13 +4,24 @@ class String
   def leetspeak()
     array = []
     split_sentence = self.split("")
-    split_sentence.each do |letter|
-      if letter.eql?("e")
-        array.push("3")     
+    split_sentence.each_with_index do |letter, index|
+      if letter.eql?("e") || letter.eql?("E")
+        array.push("3")
+      elsif letter.eql?("o") || letter.eql?("O")
+        array.push("0")
+      elsif letter.eql?("I")
+        array.push("1")
+      elsif letter.eql?("s") || letter.eql?("S")
+        if index == 0
+          array.push(letter)
+        else
+          array.push("z") || array.push("Z")
+        end
       elsif
         array.push(letter)
-      end   
+      end 
     end
-    array.join
+    array.join()
   end
 end
+
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -2,8 +2,22 @@ require ('rspec')
 require ('leetspeak')
 
 describe ('String#leetspeak') do 
-  it('replaces every "e" in a string with a "3"') do
-    expect('hello'.leetspeak).to(eq('h3llo'))
+  it('replaces every "e" or "E" in a string with a "3"') do
+    expect('beE'.leetspeak).to(eq('b33'))
   end
-  it('replaces every "o" in a string with a "0"')
+  it('replaces every "o" or "O" in a string with a "0"') do
+    expect('Oops'.leetspeak).to(eq('00pz'))
+  end
+  it('replaces every "I" in a string with a "1" but ONLY for uppercase I') do
+    expect('I am high'.leetspeak).to(eq('1 am high'))
+  end
+  it('replaces every "s" in a sring with a "z"') do 
+    expect("roses".leetspeak).to(eq("r0z3z"))
+  end
+  it('does NOT replace the first letter when it is an s') do
+    expect("saSsafrass".leetspeak).to(eq("sazzafrazz"))
+  end
+  it('replaces letters correctly in a string of words') do
+    expect("So I love writing codes so much".leetspeak).to(eq("S0 1 l0v3 writing c0d3z z0 much"))
+  end
 end
